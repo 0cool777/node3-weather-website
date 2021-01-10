72de74ddb6062e2f8d71e499faf67230a60a03c0
Roibu Vadim <roibu.vadim@gmail.com>
2021-01-10 15:16:33 +0200

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('postman-request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=8ae359b52d26c47b94433975ca416861&query='+ latitude +','+ longitude
+
+    request ({ url: url, json: true }, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            const currTemp = body.current.temperature
+            const feelTemp = body.current.feelslike
+            const weatherDescription = body.current.weather_descriptions[0]
+            callback(undefined, weatherDescription + '. It is currently ' + currTemp +' degrees out. It feels like ' + feelTemp + ' degrees out.')
+        }
+    })
+}
+
+module.exports = forecast
